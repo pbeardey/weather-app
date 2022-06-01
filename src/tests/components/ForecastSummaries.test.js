994f9487d8a2be02e5a4145ae11ae8f62d8905e4
Paul Beard <pbeardey@gmail.com>
2022-06-01 16:12:09 +0100

    ForecastSummaries component tests

---
+++ src/tests/components/ForecastSummaries.test.js
@@ -0,0 +1,66 @@
+// src/tests/components/ForecastSummaries.test.js
+
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummaries from "../../components/ForecastSummaries";
+
+describe("ForecastSummaries", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "Stub description",
+      icon: "stubIcon",
+      temperature: {
+        min: 12,
+        max: 22,
+      },
+    },
+    {
+      date: 1111111+25200000,
+      description: "Stub2 description",
+      icon: "stub2Icon",
+      temperature: {
+        min: 15,
+        max: 25,
+      },
+    }
+  ];
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("ForecastSummaries-values", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "Stub description",
+      icon: "stubIcon",
+      temperature: {
+        min: 12,
+        max: 22,
+      },
+    },
+    {
+      date: 1111111+25200000,
+      description: "Stub2 description",
+      icon: "stub2Icon",
+      temperature: {
+      min: 15,
+      max: 25,
+      },
+    }
+  ];
+
+  it("renders 2 forecasts", () => {
+    const { getAllByTestId } = render(
+      <ForecastSummaries forecasts={validProps} />
+    );
+
+    expect(getAllByTestId("forecast-summary")).toHaveLength(2);
+  });
+
+});
