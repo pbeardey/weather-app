f86b1fc28c19ac220277f7e5a8eb26c7252438bc
Paul Beard <pbeardey@gmail.com>
2022-06-03 09:36:56 +0100

    add SearchForm component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@
 import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
-import getForecast from "../requests/getForecasts";
+import getForecasts from "../requests/getForecasts";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [location, setLocation] = useState({ city: "", country: "" });
@@ -14,7 +15,7 @@ function App() {
   const [selectedDate, setSelectedDate] = useState(0);
 
   useEffect(() => {
-    getForecast(setSelectedDate, setForecasts, setLocation);
+    getForecasts(setSelectedDate, setForecasts, setLocation);
   }, []);
 
   const selectedForecast = forecasts.find(
@@ -28,6 +29,7 @@ function App() {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,15 @@
+// src/components/searchForm/js
+
+import React from "react";
+// import PropTypes from "prop-types";
+
+function SearchForm() {
+  return (
+    <div className="search-form">
+      <input type="text" />
+      <button type="submit">Search</button>
+    </div>
+  );
+}
+
+export default SearchForm;
