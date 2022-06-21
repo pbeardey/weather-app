443b035f2de93f0a929b2ec8b9aafdd38cd02753
Paul Beard <pbeardey@gmail.com>
2022-06-21 22:17:00 +0100

    refactor props

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -1,38 +1,38 @@
-// src/tests/components/ForecastSummaries.test.js
-
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("ForecastSummaries", () => {
-  const validProps = [
-    {
-      date: 1111111,
-      description: "Stub description",
-      icon: "800",
-      temperature: {
-        min: 12,
-        max: 22,
+  const validProps = {
+    propArray: [
+      {
+        date: 1111111,
+        description: "Stub description",
+        icon: "800",
+        temperature: {
+          min: 12,
+          max: 22,
+        },
       },
-    },
-    {
-      date: 26311111,
-      description: "Stub2 description",
-      icon: "802",
-      temperature: {
-        min: 15,
-        max: 25,
+      {
+        date: 26311111,
+        description: "Stub2 description",
+        icon: "802",
+        temperature: {
+          min: 15,
+          max: 25,
+        },
       },
-    },
-  ];
-  function onForecastSelect() {}
+    ],
+    propFunction: () => {},
+  };
 
   describe("ForecastSummaries-snapshot", () => {
     it("renders correctly", () => {
       const { asFragment } = render(
         <ForecastSummaries
-          forecasts={validProps}
-          onForecastSelect={onForecastSelect}
+          forecasts={validProps.propArray}
+          onForecastSelect={validProps.propFunction}
         />
       );
 
@@ -44,18 +44,19 @@ describe("ForecastSummaries", () => {
     it("renders 2 forecasts", () => {
       const { getAllByTestId } = render(
         <ForecastSummaries
-          forecasts={validProps}
-          onForecastSelect={onForecastSelect}
+          forecasts={validProps.propArray}
+          onForecastSelect={validProps.propFunction}
         />
       );
+
       expect(getAllByTestId("forecast-summary")).toHaveLength(2);
     });
 
     it("renders 2 buttons", () => {
       render(
         <ForecastSummaries
-          forecasts={validProps}
-          onForecastSelect={onForecastSelect}
+          forecasts={validProps.propArray}
+          onForecastSelect={validProps.propFunction}
         />
       );
       const buttons = screen.getAllByRole("button");
